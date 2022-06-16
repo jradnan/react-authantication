2b6088d5b0fa3edc8d1535dfa2446b526de33074
jradnan <mohd.yasin085208@gmail.com>
2022-06-16 17:05:14 +0600

    updateauthantication

---
+++ src/firebass/Firebass.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBSg3l5kTyt38Je-HKONOZcl6ckCgBl6fY",
+  authDomain: "simple-react-authanticatopn.firebaseapp.com",
+  projectId: "simple-react-authanticatopn",
+  storageBucket: "simple-react-authanticatopn.appspot.com",
+  messagingSenderId: "233283160391",
+  appId: "1:233283160391:web:8a8d77446e47d430b92462"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
